4819ef6f374bd4b44a57e89706957a87a3f5f917
mayurj1 <mayurj476@gmail.com>
2025-07-29 13:26:43 +0000

    Refactor IDbFactory methods for consistency and formatting

--- DbManager/include/IDbFactory.hpp
+++ DbManager/include/IDbFactory.hpp
@@ -10,11 +10,10 @@ public:
     virtual void closeDatabase() = 0;
     virtual void executequery(string query) = 0;
 
-    virtual void getTrackId(string songpath, int &Id){};
-    virtual void getAllSongs(vector<songinfo> &songs){};
-    virtual string getTrackPath(const string &songName){};
+    virtual void getTrackId(string songpath, int &Id) {};
+    virtual void getAllSongs(vector<songinfo> &songs) {};
+    virtual string getTrackPath(const string &songName) {return "";};
 
-   virtual  void toggeleTrackFavstate(const int id, bool state){};
-    virtual void insertSongdetail(tableData &t){};
-
+    virtual void toggeleTrackFavstate(const int id, bool state) {};
+    virtual void insertSongdetail(tableData &t) {};
 };
